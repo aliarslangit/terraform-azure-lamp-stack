7abf20f95e8690822d0c8d6a167439e55a46cbbb
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-05 21:47:20 +0500

    Added module for Azure RG

---
+++ modules/management/azure_resource_group/variables.tf
@@ -0,0 +1,10 @@
+//------------------------------------General Properties------------------------------------\\
+variable "rgname" {
+  type    = string
+  default = "Rg-git"
+}
+
+variable "location" {
+  type    = string
+  default = "West US"
+}
