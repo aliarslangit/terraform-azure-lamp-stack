280fe78f67b1324169307349897cf5a58701084f
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-06 13:25:01 +0500

    Added module for Azure VM

---
+++ modules/compute/Azure VM/main.tf
@@ -0,0 +1,59 @@
+#terraform script to create VM
+resource "azurerm_public_ip" "linuxvm" {
+  name                = "${var.vmname}-ip"
+  location            = var.location
+  resource_group_name = var.rgname
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "linuxvm" {
+  name                = "${var.vmname}-nic"
+  location            = var.location
+  resource_group_name = var.rgname
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.linuxvm.id
+
+
+  }
+}
+resource "azurerm_virtual_machine" "linuxvm" {
+  name                             = "${var.vmname}-vm"
+  location                         = var.location
+  resource_group_name              = var.rgname
+  network_interface_ids            = [azurerm_network_interface.linuxvm.id]
+  vm_size                          = var.vmsize
+  delete_os_disk_on_termination    = true #to preserve os-disk after deletion, set this poperty to false
+  delete_data_disks_on_termination = true #to preserve data-disk after deletion, set this poperty to false
+
+  #Windows or marketplace images can be used as per the requirements
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${var.vmname}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.adminuser
+    admin_password = var.adminpassword
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = var.environment
+    owner = var.owner
+    createdon = var.creationdate
+  }
+}
---
+++ modules/compute/Azure VM/variable.tf
@@ -0,0 +1,65 @@
+
+//------------------------------------General Properties------------------------------------\\
+variable "rgname" {
+  type    = string
+  default = "RG-git"
+}
+variable "vmname" {
+  type    = string
+  default = "demo-vm"
+}
+
+variable "location" {
+  type    = string
+  default = "West US"
+}
+//------------------------------------VM Configurations------------------------------------\\
+variable "adminuser" {
+  type    = string
+  default = "vmuser"
+}
+variable "adminpassword" {
+  type    = string
+  default = "*************"
+}
+variable "vmsize" {
+  type    = string
+  default = "Standard_F2"
+}
+
+variable "subnet_id" {
+  type = string
+  default = "0000000-000000000000-0000000000"
+
+}
+//------------------------------------OS Configurations-------------------------------------\\
+variable "publisher" {
+  type    = string
+  default = "Canonical"
+}
+variable "offer" {
+  type    = string
+  default = "UbuntuServer"
+}
+variable "sku" {
+  type    = string
+  default = "16.04-LTS"
+}
+
+
+//------------------------------------TAGS---------------------------------------------------\\
+variable "creationdate" {
+  type        = string
+  default     = "1/1/1111"
+  description = "description"
+}
+
+variable "environment" {
+  type    = string
+  default = "Dev"
+}
+
+variable "owner" {
+  type = string
+  default = "Ali Arslan"
+}
