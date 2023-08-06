6b216ac2cb0ca3fa63df6a946ee8061eb927df1f
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-06 14:44:13 +0500

    Update main.tf

--- modules/compute/Azure VM/main.tf
+++ modules/compute/Azure VM/main.tf
@@ -20,43 +20,26 @@ resource "azurerm_network_interface" "linuxvm" {
 
   }
 }
-resource "azurerm_virtual_machine" "linuxvm" {
-  name                             = "${var.vmname}-vm"
-  location                         = var.location
-  resource_group_name              = var.rgname
-  network_interface_ids            = [azurerm_network_interface.linuxvm.id]
-  vm_size                          = var.vmsize
-  delete_os_disk_on_termination    = true #to preserve os-disk after deletion, set this poperty to false
-  delete_data_disks_on_termination = true #to preserve data-disk after deletion, set this poperty to false
-  custom_data                       = filebase64("lamp.sh")   #Setting up LAMP
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  name                = var.vmname
+  resource_group_name = var.rgname
+  location            = var.location
+  size                = var.vmsize
+  admin_username      = var.adminuser
+  admin_password      = var.adminpassword
+  custom_data         = filebase64("lamp.sh")
+  network_interface_ids = [
+    azurerm_network_interface.linuxvm.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-  #Windows or marketplace images can be used as per the requirements
-  storage_image_reference {
+  source_image_reference {
     publisher = var.publisher
     offer     = var.offer
     sku       = var.sku
     version   = "latest"
   }
-
-  storage_os_disk {
-    name              = "${var.vmname}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = var.adminuser
-    admin_password = var.adminpassword
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = var.environment
-    owner = var.owner
-    createdon = var.creationdate
-  }
-
 }
-
