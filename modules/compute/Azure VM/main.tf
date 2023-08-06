64efb53a31c8d3fe5598a571cf1b9196adf4ea79
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-06 20:03:55 +0500

    Update main.tf

--- modules/compute/Azure VM/main.tf
+++ modules/compute/Azure VM/main.tf
@@ -1,4 +1,36 @@
 #terraform script to create VM
+
+locals {
+  custom_data = <<CUSTOM_DATA
+ #!/bin/bash
+
+#Install Apache
+sudo apt update
+sudo apt install apache2 -y
+
+#Allow apache from firewall
+sudo ufw app list
+sudo ufw allow in "Apache"
+sudo ufw status
+
+#Install mysql
+sudo apt install mysql-server -y
+
+
+#Install php
+sudo apt install php libapache2-mod-php php-mysql -y
+
+#Verify Installations
+ php -v
+ apache2 -v
+ sudo mysql
+
+# Output LAMP stack details
+echo "LAMP stack installation complete!"
+echo "Apache installed and running."
+echo "PHP installed and configured."
+  CUSTOM_DATA
+  }
 resource "azurerm_public_ip" "linuxvm" {
   name                = "${var.vmname}-ip"
   location            = var.location
@@ -27,35 +59,8 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   size                = var.vmsize
   admin_username      = var.vmuser
   admin_password      = var.vmpassword
-  custom_data = <<-EOT
-#!/bin/bash
+  custom_data = base64encode(local.custom_data)
 
-#Install Apache
-sudo apt update
-sudo apt install apache2 -y
-
-#Allow apache from firewall
-sudo ufw app list
-sudo ufw allow in "Apache"
-sudo ufw status
-
-#Install mysql
-sudo apt install mysql-server -y
-
-
-#Install php
-sudo apt install php libapache2-mod-php php-mysql -y
-
-#Verify Installations
- php -v
- apache2 -v
- sudo mysql
-
-# Output LAMP stack details
-echo "LAMP stack installation complete!"
-echo "Apache installed and running."
-echo "PHP installed and configured."
-  EOT
   network_interface_ids = [
     azurerm_network_interface.linuxvm.id,
   ]
