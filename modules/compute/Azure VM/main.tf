cb967d56d4f99374ae6347e9214029890844b491
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-06 20:33:52 +0500

    Update main.tf

--- modules/compute/Azure VM/main.tf
+++ modules/compute/Azure VM/main.tf
@@ -48,8 +48,6 @@ resource "azurerm_network_interface" "linuxvm" {
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.linuxvm.id
-
-
   }
 }
 resource "azurerm_linux_virtual_machine" "linuxvm" {
